f242bd74695146ff1cb861652aa5ca7b87e7fee4
kntchrstn <kentchristianluardo@gmail.com>
2025-09-29 16:11:27 +0800

    changing the background color of the dashboard

--- frontend/src/layout/DashboardLayout.jsx
+++ frontend/src/layout/DashboardLayout.jsx
@@ -28,7 +28,7 @@ const DashboardLayout = () => {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex justify-between border-b bg-gray backdrop-blur-lg h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <header className="flex justify-between border-b h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 header-bg-white">
             <div className="flex items-center gap-2 px-4">
               <SidebarTrigger className="-ml-1" />
               <Separator orientation="vertical" className="mr-2 h-4" />
